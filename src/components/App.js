013ccdc014bd2ac915843d642997bf0525257fc1
Mr-R-e-X <souvikhazra151@gmail.com>
2024-08-01 15:13:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,105 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    make: "",
+    model: "",
+  });
 
-export default App
+  const handleNext = (val) => {
+    setStep(step + val);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      make: "",
+      model: "",
+    });
+    setStep(1);
+  };
+
+  return (
+    <div id="main">
+      <form onSubmit={(event) => handleSubmit(event)}>
+        {step === 1 && (
+          <>
+            <div>
+              <label for="firstName">First name</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="Firstname"
+                // value={formData.firstName}
+                onChange={(e) =>
+                  setFormData({ ...formData, firstName: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label for="lastName">Last name</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                placeholder="Lastname"
+                // value={formData.lastName}
+                onChange={(e) =>
+                  setFormData({ ...formData, lastName: e.target.value })
+                }
+              />
+            </div>
+            <button type="button" onClick={() => handleNext(1)}>
+              Next
+            </button>
+          </>
+        )}
+        {step === 2 && (
+          <>
+            <div>
+              <label for="make">Make</label>
+              <input
+                type="text"
+                name="make"
+                id="make"
+                placeholder="make"
+                // value={formData.make}
+                onChange={(e) =>
+                  setFormData({ ...formData, make: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label for="model">Model</label>
+              <input
+                type="text"
+                name="model"
+                id="model"
+                placeholder="model"
+                // value={formData.model}
+                onChange={(e) =>
+                  setFormData({ ...formData, model: e.target.value })
+                }
+              />
+            </div>
+
+            <button type="button" onClick={() => handleNext(-1)}>
+              Previous
+            </button>
+            <button type="submit">Submit</button>
+          </>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default App;
